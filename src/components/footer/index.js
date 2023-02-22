ece416ab8569181a98fda4ddb1c49433d89ef305
manishsundriyal <msundriyal27@gmail.com>
2023-02-22 17:49:48 +0530

    update footer links, address

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { faFacebookF, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./index.scss";
 
 const Footer = () => {
@@ -10,8 +12,14 @@ const Footer = () => {
           <div className="description">
             <strong>Address:</strong>
             <br />
-            1st floor, sikka cham, B-Block, dda market, C-Block, Dilshad Garden,
+            1st floor, Sikka Cham, B Block, DDA Market, Dilshad Garden,
             Delhi, 110095
+            <br />
+            <strong>Mobile No: </strong>
+            9999777798
+            <br />
+            <strong>Email: </strong>
+            squat2fit@squat2fit.in
           </div>
         </div>
         <div className="navigation footer_item">
@@ -27,8 +35,17 @@ const Footer = () => {
         <div className="contact footer_item">
           <div className="wrapper">
             <h3 className="title">Contact</h3>
-            <span>FB</span>
-            <span>IG</span>
+            <div className="icon_wrapper">
+              <a className="social_icon facebook" href="https://www.facebook.com/pages/Squat2fit-Unisex-gym/113054150112092" target="_blank">
+                <FontAwesomeIcon icon={faFacebookF}/>
+              </a>
+              <a className="social_icon instagram" href="https://www.instagram.com/squat2fit/" target="_blank">
+                <FontAwesomeIcon icon={faInstagram}/>
+              </a>
+              <a className="social_icon whatsapp" href="https://api.whatsapp.com/send?phone=9999777798&text=Hello" target="_blank">
+                <FontAwesomeIcon icon={faWhatsapp}/>
+              </a>
+            </div>
           </div>
         </div>
       </div>
